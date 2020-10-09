d1f78b1190f690fe035e872947c023735a93685c
Demange <aymeric.demange@ubisoft.com>
2020-10-09 16:49:50 +0200

    WIP : pushing first auth

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = ({}) => {
   return (
     <div>
-      Home
+      <a href="https://accounts.spotify.com/authorize?client_id=23d0c774be854502a7e0d5c415e2c29c&response_type=token&redirect_uri=https://localhost:3000">Authentification</a>
     </div>
   )
 }
