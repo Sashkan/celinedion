72d730e8abb7e1bd1a0aa5a038e4f3f363b13f1f
Rebecca <rebecca.dubois-romo@ubisoft.com>
2020-10-16 17:22:15 +0200

    feat: spotify login button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,60 @@
-import React from 'react'
-import Axios from 'axios'
+import React, { useEffect, useState } from 'react';
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "62f8fcfd04454706a6a049566aafe0fb";
+const redirectUri = "http://localhost:3000";
+const scopes = [
+  "streaming",
+  "user-read-email",
+  "user-read-private",
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
+
+// Get the hash of the url
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+window.location.hash = "";
+
+export default function Home () {
+  const [ token, setToken] = useState(null);
+  useEffect(() => {
+    let _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+  }, [])
+
+
+  return (
+    <div className="Home">
+      {!token && (
+        <a
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+        >
+          Login to Spotify
+        </a>
+      )}
+      {token && (
+        "// Spotify Player Will Go Here In the Next Step"
+      )}
+    </div>
+  );
+}
+
+
+/*import Axios from 'axios'
 
 const Home = ({}) => {
   const getMe=()=>{
@@ -16,4 +71,4 @@ const Home = ({}) => {
   )
 }
 
-export default Home
+export default Home*/
